4247bf2ce9974f77b6e416288384ea7ddb9fe428
Alexander <zander2001@hotmail.se>
2021-02-14 22:44:15 +0100

    first commit hope it works

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BinanceBTCTrader
+
+Se till att du alltid har mer än 100 dollar i värde på binance då varje trade måste vara med minst 10 dollar
+
+```node index.js``` För att starta
---
+++ index.js
@@ -0,0 +1,56 @@
+recuire('dotenv').config;
+const ccxt = recuire('ccxt');
+const axios = recuire('axios');
+
+
+const tick = async (config, binanceClient) => {
+    const { asset, base, spred, allocation } = config;
+    const market = `${asset}/${base}`;
+
+    const orders = await binanceClient.fetchOpenOrders(market);
+    orders.forEach(async order => {
+        await binanceClient.cancelOrder(order.id, order.symbol);
+    });
+
+    const results = await Promise.all([
+        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'),
+        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd')
+    ]);
+    const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
+
+    const sellPrice = marketPrice * (1 + spred);
+    const buyPrice = marketPrice * (1 - spred);
+    const balances = await binanceClient.fetchBalance();
+    const assetBalance = balances.free[asset];
+    const baseBalance = balances.free[base];
+    const sellVolume = assetBalance * allocation;
+    const buyVolume = (baseBalance * allocation) / marketPrice;
+
+    await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
+    await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
+
+    console.log(`
+    New tick for ${market}
+    Created limit sell order for ${sellVolume}@${sellPrice}
+    Create limit buy order for ${buyVolume}@${buyPrice}
+    `);
+};
+
+
+const run = () => {
+    const config = {
+        asset: 'BTC',
+        base: 'USDT',
+        allocation: 0.1,
+        spred: 0.2,
+        tickInterval: 2000
+    };
+    const binanceClient = new ccxt.binance({
+        apiKey: process.env.API_KEY,
+        apiSecret: process.env.API_SECRET
+    });
+    tick(config, binanceClient);
+    setInterval(tick, config.tickInterval, config, binanceClient);
+};
+
+run();
